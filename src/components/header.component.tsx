bc3ffeaab93d3f4eff4931f0f345f544ae5b2d37
Sheharyar566 <Sheharyar566@gmail.com>
2023-02-21 15:30:52 +0500

    Fixed text colors

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -7,8 +7,8 @@ import { GrMail } from 'react-icons/gr';
 const Header = () => {
     return (
         <header className="bg-blobPattern bg-secondary bg-no-repeat bg-cover bg-origin-border min-h-screen flex flex-col justify-center items-center relative">
-            <h1 className='text-center text-5xl mb-2 font-Roboto'>Shehar Yar Khan</h1>
-            <p className="text-xl mb-10">Full Stack Developer</p>
+            <h1 className='text-center text-5xl mb-2 text-white font-Roboto'>Shehar Yar Khan</h1>
+            <p className="text-xl text-white mb-10">Full Stack Developer</p>
 
             <div className="flex flex-row justify-center gap-4 items-center">
                 <a href="https://linkedin.com/in/shehar-yar-khan"><FaLinkedin className="text-white text-3xl" /></a>
@@ -16,9 +16,9 @@ const Header = () => {
                 <a href="mailto:sheharyar566@gmail.com"><GrMail className="text-white text-3xl" /></a>
             </div>
 
-            <Link href="#projects" className='absolute bottom-4 flex flex-col justify-center items-center'>
-                <p className="text-xl font-bold">Projects</p>
-                <BiChevronDown className="text-2xl" />
+            <Link href="#projects" className='animate-bounce absolute bottom-4 flex flex-col justify-center items-center'>
+                <p className="text-xl text-white font-bold">Projects</p>
+                <BiChevronDown className="text-2xl text-white" />
             </Link>
         </header>
     );
